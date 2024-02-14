51dd3caba7633154d7900316684dfdd645f40d9e
Pedroo <pedrohenriquemoalive@gmail.com>
2024-02-14 09:23:35 -0300

    Criando  components do  front-end

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './App.jsx'
 import './index.css'
+import {Login} from './components/Login'
+import {Register} from './components/Register'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import {
+  createBrowserRouter,
+  RouterProvider
+} from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Login/>
+      },
+      {
+        path: "register",
+        element: <Register/>
+      },
+    ]
+  }
+])
+
